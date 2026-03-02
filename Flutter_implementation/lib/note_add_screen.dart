a3678cb78bf56d5f459322202360bf49567befca
kevin <ktdnguyen@uia.no>
2026-03-02 17:01:32 +0100

    Fixing bug

--- Flutter_implementation/lib/note_add_screen.dart
+++ Flutter_implementation/lib/note_add_screen.dart
@@ -16,34 +16,36 @@ class _NoteAddScreenState extends State<NoteAddScreen> {
       appBar: AppBar(
         title: const Text('Add Note'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Title'),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _contentController,
-              decoration: const InputDecoration(labelText: 'Content'),
-              maxLines: 5,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle saving the note here
-                String title = _titleController.text;
-                String content = _contentController.text;
-                print('Saving note: $title - $content');
-                Navigator.pop(context, {'title': title, 'content': content});
-              },
-              child: const Text('Save Note'),
-            ),
-          ],
+      body: SingleChildScrollView(  
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _contentController,
+                decoration: const InputDecoration(labelText: 'Content'),
+                maxLines: 5,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  String title = _titleController.text;
+                  String content = _contentController.text;
+                  print('Saving note: $title - $content');
+                  Navigator.pop(context, {'title': title, 'content': content});
+                },
+                child: const Text('Save Note'),
+              ),
+            ],
+          ),
         ),
       ),
+      resizeToAvoidBottomInset: true, 
     );
   }
 }
